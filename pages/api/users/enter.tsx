6dcd375955775b9e401d55e41b833e16bd4dd3d7
JunDemi <jungwook3176@gmail.com>
2023-04-13 16:09:31 +0900

    상품등록, 관련상품, 찜하기

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -53,7 +53,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 }
 
 export default withHandler({
-  method: "POST",
+  methods: ["POST"],
   handler,
   isPrivate: false
 });
